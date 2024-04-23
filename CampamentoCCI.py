635a4c23e3e96d260444f7d4ec700b901326abc5
Carlos Banegas <cbanegas@unitec.edu>
2024-04-22 20:59:29 -0600

    name change

--- CampamentoCCI.py
+++ CampamentoCCI.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 
 # Page setup
-st.set_page_config(page_title="Campamento Juvenil CCI 2023", page_icon="🐍", layout="wide")
-st.title("Campamento Juvenil CCI 2023")
+st.set_page_config(page_title="Cena de Gala Conmemorativa 25 Aniversario", page_icon="🐍", layout="wide")
+st.title("Cena de Gala Conmemorativa 25 Aniversario")
 
 # Connect to the Google Sheet
 sheet_id = "1wvbS-5IYJOEtoww-4PnC-uJDCe6NIvhCrlrMEvc5VDM"
